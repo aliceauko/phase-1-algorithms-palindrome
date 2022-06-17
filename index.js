f4826b64c837798a01dca53511aa6125b9ab9784
aliceauko <alice.auko@student.moringaschool.com>
2022-06-17 07:44:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-function isPalindrome(word) {
+function reverse(word) {
   // Write your algorithm here
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
-*/
+*/ function isPalindrome(word){
+  const reversedWord = reverse(word)
+  return word === reversedWord;
+}
 
 /*
   Add written explanation of your solution here
 */
+// the reversed word should be same as the word input for it to be true e.g mom===mom
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
